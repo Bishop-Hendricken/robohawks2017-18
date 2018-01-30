34c629e7da268e4ca91ef81a3b218e0cdc47b42b
Paarth Tandon <paarthri@gmail.com>
2018-01-30 15:17:07 -0500

    Finished GrabandLift

--- TeamCode/src/main/java/robohawks/controllers/ContestTeleOpController.java
+++ TeamCode/src/main/java/robohawks/controllers/ContestTeleOpController.java
@@ -15,16 +15,14 @@ import robohawks.modules.base.TurnModule;
 public class ContestTeleOpController extends Controller{
 
     HolonomicDriveModule drive;
-    DcMotor arm;
-    CRServo leftServo;
-    Servo rightServo;
+    DcMotor leftarm;
+    DcMotor rightarm;
 
     @Override
     public void init() {
         drive = new HolonomicDriveModule(hardwareMap);
-        arm = hardwareMap.dcMotor.get("arm");
-        leftServo = hardwareMap.crservo.get("left");
-        rightServo = hardwareMap.servo.get("right");
+        leftarm = hardwareMap.dcMotor.get("leftarm");
+        rightarm = hardwareMap.dcMotor.get("rightarm");
     }
 
     @Override
@@ -33,59 +31,85 @@ public class ContestTeleOpController extends Controller{
 
         //Drive
 
-        float gamepad1LeftY = gamepad1.left_stick_y;
-        float gamepad1LeftX = -gamepad1.left_stick_x;
-        float gamepad1RightX = -gamepad1.right_stick_x;
+        if (gamepad1.dpad_up){
+            drive.setPowerOne(.5);
+            drive.setPowerTwo(.5);
+            drive.setPowerThree(.5);
+            drive.setPowerFour(.5);
+        } else if (gamepad1.dpad_down) {
+            drive.setPowerOne(-.5);
+            drive.setPowerTwo(-.5);
+            drive.setPowerThree(-.5);
+            drive.setPowerThree(-.5);
+        } else if (gamepad1.dpad_left){
+            drive.setPowerOne(.5);
+            drive.setPowerTwo(-.5);
+            drive.setPowerThree(-.5);
+            drive.setPowerFour(.5);
+        } else if (gamepad1.dpad_right){
+            drive.setPowerOne(-.5);
+            drive.setPowerTwo(.5);
+            drive.setPowerThree(.5);
+            drive.setPowerFour(-.5);
+        } else {
+            drive.setPowerOne(0);
+            drive.setPowerTwo(0);
+            drive.setPowerThree(0);
+            drive.setPowerFour(0);
+        }
 
-        float FrontLeft = -gamepad1LeftY - gamepad1LeftX - gamepad1RightX;
-        float FrontRight = gamepad1LeftY - gamepad1LeftX - gamepad1RightX;
-        float BackRight = gamepad1LeftY + gamepad1LeftX - gamepad1RightX;
-        float BackLeft = -gamepad1LeftY + gamepad1LeftX - gamepad1RightX;
+        if(gamepad1.right_stick_x > 1){
+            drive.setPowerOne(.5);
+            drive.setPowerTwo(.5);
+            drive.setPowerThree(.5);
+            drive.setPowerFour(.5);
+        } else if (gamepad1.right_stick_x < 0){
+            drive.setPowerOne(-.5);
+            drive.setPowerTwo(-.5);
+            drive.setPowerThree(-.5);
+            drive.setPowerFour(-.5);
+        } else {
+            drive.setPowerOne(0);
+            drive.setPowerTwo(0);
+            drive.setPowerThree(0);
+            drive.setPowerFour(0);
+        }
 
-        FrontRight = Range.clip(FrontRight, -1, 1);
-        FrontLeft = Range.clip(FrontLeft, -1, 1);
-        BackLeft = Range.clip(BackLeft, -1, 1);
-        BackRight = Range.clip(BackRight, -1, 1);
-
-        drive.setPowerTwo(FrontRight/2);
-        drive.setPowerOne(FrontLeft/2);
-        drive.setPowerThree(BackLeft/2);
-        drive.setPowerFour(BackRight/2);
+//        float gamepad1LeftY = gamepad1.left_stick_y;
+//        float gamepad1LeftX = -gamepad1.left_stick_x;
+//        float gamepad1RightX = -gamepad1.right_stick_x;
+//
+//        float FrontLeft = -gamepad1LeftY - gamepad1LeftX - gamepad1RightX;
+//        float FrontRight = gamepad1LeftY - gamepad1LeftX - gamepad1RightX;
+//        float BackRight = gamepad1LeftY + gamepad1LeftX - gamepad1RightX;
+//        float BackLeft = -gamepad1LeftY + gamepad1LeftX - gamepad1RightX;
+//
+//        FrontRight = Range.clip(FrontRight, -1, 1);
+//        FrontLeft = Range.clip(FrontLeft, -1, 1);
+//        BackLeft = Range.clip(BackLeft, -1, 1);
+//        BackRight = Range.clip(BackRight, -1, 1);
+//
+//        drive.setPowerTwo(FrontRight/2);
+//        drive.setPowerOne(FrontLeft/2);
+//        drive.setPowerThree(BackLeft/2);
+//        drive.setPowerFour(BackRight/2);
 
         //Arm
 
         if (gamepad2.dpad_down){
-            arm.setPower(.5);
+            leftarm.setPower(.5);
+            rightarm.setPower(.5);
         } else {
-            arm.setPower(0);
+            leftarm.setPower(0);
+            rightarm.setPower(0);
         }
 
         if (gamepad2.dpad_up){
-            arm.setPower(-.5);
+            leftarm.setPower(-.5);
+            rightarm.setPower(-.5);
         } else {
-            arm.setPower(0);
-        }
-
-        //Grab
-
-        if (gamepad2.right_bumper){
-            rightServo.setPosition(.45);
-        }
-
-        if (gamepad2.right_trigger > 0){
-            rightServo.setPosition(.75);
-        }
-
-        if (gamepad2.right_stick_x < 0){
-            leftServo.setPower(1);
-        }else{
-            leftServo.setPower(0);
-        }
-
-        if (gamepad2.right_stick_x > 0){
-            leftServo.setPower(-1);
-        }else{
-            leftServo.setPower(0);
+            leftarm.setPower(0);
+            rightarm.setPower(0);
         }
 
     }
